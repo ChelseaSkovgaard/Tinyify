f3f2ce49b08f9a90959a0b3e7db2640f7114fe1f
MilkMan90 <mlkaufman747@gmail.com>
2017-01-24 15:05:47 -0700

    more api setup

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 
 class LinkList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      urls: []
+    }
+  }
   componentDidMount(){
-    fetch(`/urls`)
+    fetch(`/api/folders`)
       .then((res)=>{
         console.log(res);
         return res.json()
       })
       .then((res)=>{
+        this.setState({urls:res});
         console.log(res);
       });
   }
   render() {
+  
     return (
       <div className="LinkList">
 
