b5269a15664bb5d1dbded251f9a75cd7d070d617
MilkMan90 <mlkaufman747@gmail.com>
2017-01-27 10:40:19 -0700

    env variable

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.HEROKU_POSTGRESQL_SILVER_URL + `?ssl=true`,    migrations: {
+    connection: process.env.DATABASE_URL + `?ssl=true`,    migrations: {
       directory: './db/migrations'
     },
     seeds: {
